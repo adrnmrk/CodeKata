ef8bb392652d995e5991af1854e14493d422af09
Adrian Mark Dalipe <50126402+adrnmrk@users.noreply.github.com>
2021-03-05 17:51:38 +0800

    added ArraysExercise1

---
+++ src/codekata/easy/ArraysExercise1.java
@@ -0,0 +1,32 @@
+package codekata.easy;
+
+import java.util.Scanner;
+
+/**
+ * Make an array that holds the textual values of the days of the week.
+ * User input a number corresponding to the day of the week; week stats on monday
+ * Output the string that represents the day of the week
+ */
+public class ArraysExercise1 {
+	
+	    private static final Scanner scanner = new Scanner(System.in);
+
+	    public static void main(String[] args) {
+			try {
+				int number;
+				System.out.println("Enter a number from 1 to 7: ");
+				number = scanner.nextInt();
+				scanner.close();
+				String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+				//number -=1;
+				System.out.println(daysOfWeek[number - 1]);
+				}
+			catch (Exception e){
+				System.out.println("Invalid Input!");
+			}
+
+	    }
+
+	}
+
+
